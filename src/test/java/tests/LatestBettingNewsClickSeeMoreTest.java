a63029629f565e2519bea1b41865319cddb3b467
Norbert00 <norbert.jurek@protonmail.com>
2020-02-18 21:16:17 +0100

    Second test case

--- src/test/java/tests/LatestBettingNewsClickSeeMoreTest.java
+++ src/test/java/tests/LatestBettingNewsClickSeeMoreTest.java
@@ -1,4 +1,19 @@
 package tests;
 
-public class LatestBettingNewsClickSeeMoreTest {
+import org.testng.annotations.Test;
+import pop.main.LatestBettingNews;
+import static org.testng.Assert.assertTrue;
+
+public class LatestBettingNewsClickSeeMoreTest extends TestBase {
+
+
+    @Test
+    public void userClickSeeMoreButtonTest() {
+        LatestBettingNews news = new LatestBettingNews();
+        news.clickOnSeeMoreButton(news.getSeeMoreButton());
+
+        assertTrue(news.isVisibleLoadedElement(news.getLoadedElement()));
+    }
+
+
 }
