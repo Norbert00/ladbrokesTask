19bff923536e86da5729838522525c90cf8585b8
Norbert <231>
2020-02-19 15:02:34 +0100

    Latest Betting News test

--- src/test/java/tests/LatestBettingNewsClickSeeMoreTest.java
+++ src/test/java/tests/LatestBettingNewsClickSeeMoreTest.java
@@ -2,6 +2,8 @@ package tests;
 
 import org.testng.annotations.Test;
 import pop.main.LatestBettingNews;
+import waits.WaitForElement;
+
 import static org.testng.Assert.assertTrue;
 
 public class LatestBettingNewsClickSeeMoreTest extends TestBase {
@@ -10,9 +12,10 @@ public class LatestBettingNewsClickSeeMoreTest extends TestBase {
     @Test
     public void userClickSeeMoreButtonTest() {
         LatestBettingNews news = new LatestBettingNews();
-        news.clickOnSeeMoreButton(news.getSeeMoreButton());
+        news.clickOnSeeMoreButton(1,news.getSeeMoreButtons());
 
-        assertTrue(news.isVisibleLoadedElement(news.getLoadedElement()));
+        assertTrue(news.loadedElementInSectionBeetingNews());
+
     }
 
 
