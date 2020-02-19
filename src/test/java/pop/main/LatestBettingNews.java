33da8cd71f9162444226f41f3259496531d298d8
Norbert <231>
2020-02-19 15:01:09 +0100

    WebElement and methods for Latest Betting News section

--- src/test/java/pop/main/LatestBettingNews.java
+++ src/test/java/pop/main/LatestBettingNews.java
@@ -2,11 +2,14 @@ package pop.main;
 
 import driver.manager.DriverManager;
 import logger.StepLogger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import waits.WaitForElement;
+
+import java.util.List;
 
 
 public class LatestBettingNews {
@@ -16,50 +19,68 @@ public class LatestBettingNews {
         PageFactory.initElements(DriverManager.getWebDriver(), this);
     }
 
-
-
-    @FindBy(xpath = "//div[@class='see-more mobile-hidden cf']/div")
-    private WebElement seeMoreButton;
-
-    @FindBy(xpath = "//a[@href='https://news.ladbrokes.com/football/kevin-doyle-on-the-premier-league-so-far-man-united-v-wolves-spurs-v-man-city.html']]")
-    private WebElement loadedElement;
-
-
-    public WebElement getSeeMoreButton() {
-        return seeMoreButton;
+    public List<WebElement> getSeeMoreButtons() {
+        return seeMoreButtons;
     }
 
-    public WebElement getLoadedElement() {
-        return loadedElement;
+    private List<WebElement> seeMoreButtons = getAllSeeMoreButtons();
+
+    // method which assign all see more buttons to the list of elements
+    private List<WebElement> getAllSeeMoreButtons() {
+        List<WebElement> list;
+        list = DriverManager.getWebDriver().findElements(By.xpath("//*[@class='h3']"));
+        return list;
     }
 
-
-    public void clickOnSeeMoreButton(WebElement element) {
-        WaitForElement.waitUntilElementIsVisible(element);
-        StepLogger.setLoggerInfo("User click on the See more button in the Latest Betting News section");
-        element.click();
-    }
-
-    public boolean isVisibleLoadedElement(WebElement element){
-        boolean isVisible;
-        if(WaitForElement.waitUntilElementIsVisible(element)) {
-            isVisible = true;
-        } else {
-            isVisible = false;
+    // method allows to click on the chosen See More button
+    public void clickOnSeeMoreButton(int numberOfTheButton, List<WebElement> elements) {
+        switch (numberOfTheButton) {
+            case 1:
+                WaitForElement.waitUntilElementIsClickable(elements.get(0));
+                elements.get(0).click();
+                StepLogger.setLoggerInfo("User clicked on the first See more button");
+                    break;
+            case 2: elements.get(1).click();
+                StepLogger.setLoggerInfo("User clicked on the second See more button");
+                    break;
+            case 3: elements.get(2).click();
+                StepLogger.setLoggerInfo("User clicked on the third See more button");
+                    break;
+            case 4: elements.get(3).click();
+                StepLogger.setLoggerInfo("User clicked on the fourth See more button");
+                    break;
+            case 5: elements.get(4).click();
+                StepLogger.setLoggerInfo("User clicked on the fifth See more button");
+                    break;
+            case 6: elements.get(5).click();
+                StepLogger.setLoggerInfo("User clicked on the sixth See more button");
+                    break;
+            default:
+                System.out.println("No elements in the array");
+                    break;
         }
-        return isVisible;
     }
 
 
+    public int getSizeOfElements() {
+        return sizeOfElements;
+    }
 
+    private int sizeOfElements = listOfElement().size();
 
-//    public List<WebElement> getAllSeeMoreButtons() {
-//        return DriverManager.getWebDriver().findElements(By.xpath("//div[@class='h3']"));
-//    }
-//    private List<WebElement> allSeeMoreButtons = getAllSeeMoreButtons();
-//    public List<WebElement> someList = DriverManager.getWebDriver().findElements(By.xpath("//div[@class='widget-frame cf']//div//div[@class='see-more desktop-hidden cf']//div[@class='h3'][contains(text(),'See more')]"));
+    private List<WebElement> listOfElement() {
+        List<WebElement> list;
+        list = DriverManager.getWebDriver().findElements(By.xpath("//*[@class='post-brick d-1of2 t-1of2 m-all']"));
+        return list;
+    }
 
-
+    public boolean loadedElementInSectionBeetingNews() {
+        if(getSizeOfElements() > 25) {
+            return true;
+        }else {
+            return false;
+        }
+    }
 
 
 
