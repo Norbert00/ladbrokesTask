dbed7a4eb528f200b72a66bb6cc1066780529363
Norbert00 <norbert.jurek@protonmail.com>
2020-02-19 18:47:42 +0100

    WebElement and methods to the Latest Betting News

--- src/test/java/pop/main/LatestBettingNews.java
+++ src/test/java/pop/main/LatestBettingNews.java
@@ -2,11 +2,12 @@ package pop.main;
 
 import driver.manager.DriverManager;
 import logger.StepLogger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import waits.WaitForElement;
+import java.util.List;
+
 
 
 public class LatestBettingNews {
@@ -17,51 +18,62 @@ public class LatestBettingNews {
     }
 
 
+    private List<WebElement> allSeeMoreButtons = catchAllSeeMoreButtons();
+    private List<WebElement> allBricksNews = catchAllBricksNews();
 
-    @FindBy(xpath = "//div[@class='see-more mobile-hidden cf']/div")
-    private WebElement seeMoreButton;
-
-    @FindBy(xpath = "//a[@href='https://news.ladbrokes.com/football/kevin-doyle-on-the-premier-league-so-far-man-united-v-wolves-spurs-v-man-city.html']]")
-    private WebElement loadedElement;
-
-
-    public WebElement getSeeMoreButton() {
-        return seeMoreButton;
+    public List<WebElement> getAllSeeMoreButtons() {
+        return allSeeMoreButtons;
     }
-
-    public WebElement getLoadedElement() {
-        return loadedElement;
+    public List<WebElement> getAllBricksNews() {
+        return allBricksNews;
     }
 
 
-    public void clickOnSeeMoreButton(WebElement element) {
-        WaitForElement.waitUntilElementIsVisible(element);
-        StepLogger.setLoggerInfo("User click on the See more button in the Latest Betting News section");
-        element.click();
+    private List<WebElement> catchAllBricksNews() {
+        List<WebElement> list;
+        list = DriverManager.getWebDriver().findElements(By.xpath("//div[@class='post-list-frame mobile-hidden cf']/div"));
+        return list;
     }
 
-    public boolean isVisibleLoadedElement(WebElement element){
-        boolean isVisible;
-        if(WaitForElement.waitUntilElementIsVisible(element)) {
-            isVisible = true;
-        } else {
-            isVisible = false;
+    private List<WebElement> catchAllSeeMoreButtons() {
+        List<WebElement> list;
+        list = DriverManager.getWebDriver().findElements(By.xpath("//div[@class='see-more mobile-hidden cf']/div"));
+        return list;
+    }
+
+
+    public void clickOnSeeMoreButton(int indexOfElement, List<WebElement> e) {
+        switch (indexOfElement) {
+            case 1:
+                WaitForElement.waitUntilElementIsVisible(e.get(0));
+                e.get(0).click();
+                StepLogger.setLoggerInfo("User has clicked on the first See More button");
+                break;
+            case 2:
+                WaitForElement.waitUntilElementIsVisible(e.get(1));
+                e.get(1).click();
+                StepLogger.setLoggerInfo("User has clicked on the second See More button");
+                break;
+            case 3:
+                WaitForElement.waitUntilElementIsVisible(e.get(2));
+                e.get(2).click();
+                StepLogger.setLoggerInfo("User has clicked on the third See More button");
+                break;
+            default:
+                System.out.println("Empty array");
         }
-        return isVisible;
     }
 
 
-
-
-//    public List<WebElement> getAllSeeMoreButtons() {
-//        return DriverManager.getWebDriver().findElements(By.xpath("//div[@class='h3']"));
-//    }
-//    private List<WebElement> allSeeMoreButtons = getAllSeeMoreButtons();
-//    public List<WebElement> someList = DriverManager.getWebDriver().findElements(By.xpath("//div[@class='widget-frame cf']//div//div[@class='see-more desktop-hidden cf']//div[@class='h3'][contains(text(),'See more')]"));
-
-
-
-
+    public boolean loadedNewsCheck(List<WebElement> e) {
+        if (e.size() > 25) {
+            StepLogger.setLoggerInfo("Bricks with news have been loaded ");
+            return true;
+        } else {
+            StepLogger.setLoggerInfo("loadedNewsChecked failed");
+            return false;
+        }
+    }
 
 }
 
